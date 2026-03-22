03cb8d939acd6d949cbae7956422e38ca35a186f
blgzm18-collab <blgzm18@gmail.com>
2026-03-21 22:24:24 -0400

    Update FocusImage.js

--- focus/src/image/FocusImage.js
+++ focus/src/image/FocusImage.js
@@ -1,2 +1,16 @@
+import { loadImage } from "../utils/canvas.js";
 
+export class FocusImage {
+  constructor(canvas) {
+    this.canvas = canvas;
+    this.ctx = canvas.getContext("2d");
+    this.image = null;
+  }
 
+  async load(file) {
+    this.image = await loadImage(file);
+    this.canvas.width = this.image.width;
+    this.canvas.height = this.image.height;
+    this.ctx.drawImage(this.image, 0, 0);
+  }
+}
