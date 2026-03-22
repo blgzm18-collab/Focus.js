f0495ae2da460a73131930672fe73457b19b9556
blgzm18-collab <blgzm18@gmail.com>
2026-03-21 22:23:44 -0400

    Update focus.js

--- focus/src/focus.js
+++ focus/src/focus.js
@@ -1,1 +1,7 @@
+import { FocusImage } from "./image/FocusImage.js";
+import { FocusVideo } from "./video/FocusVideo.js";
 
+export const focus = {
+  image: (canvas) => new FocusImage(canvas),
+  video: (videoElement, canvas) => new FocusVideo(videoElement, canvas)
+};
