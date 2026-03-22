e6cce8cc207e42b26d2ae8c2b5a2f53af96a5a5f
blgzm18-collab <blgzm18@gmail.com>
2026-03-21 22:24:42 -0400

    Update canva.js

--- focus/src/utils/canva.js
+++ focus/src/utils/canva.js
@@ -1,1 +1,15 @@
+export function loadImage(file) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
 
+    reader.onload = () => {
+      const img = new Image();
+      img.onload = () => resolve(img);
+      img.onerror = reject;
+      img.src = reader.result;
+    };
+
+    reader.onerror = reject;
+    reader.readAsDataURL(file);
+  });
+}
