6cadde4c9f891f72603abfc6593e17290ecbf79a
blgzm18-collab <blgzm18@gmail.com>
2026-03-21 22:34:27 -0400

    Update demo.js

--- demo/demo.js
+++ demo/demo.js
@@ -1,1 +1,18 @@
+import { FocusImage } from "../src/image/FocusImage.js";
 
+const canvas = document.getElementById("previewCanvas");
+const input = document.getElementById("imageInput");
+
+const editor = new FocusImage(canvas);
+
+input.addEventListener("change", async e => {
+  const file = e.target.files[0];
+  if (file) {
+    await editor.load(file);
+  }
+});
+
+// Highlight mode
+canvas.addEventListener("mousedown", e => editor.startHighlight(e));
+canvas.addEventListener("mousemove", e => editor.updateHighlight(e));
+canvas.addEventListener("mouseup", () => editor.finishHighlight());
